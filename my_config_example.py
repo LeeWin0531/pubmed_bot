00e311a47eda77b20162d9d315318cbd86ad9c39
lee <ahmulwh@163.com>
2026-02-08 02:53:07 +0800

    pubmed_bot_beta

--- my_config_example.py
+++ my_config_example.py
@@ -6,19 +6,19 @@ yesterday_str = yesterday.strftime("%Y/%m/%d")
 
 
 # --- 1. DeepSeek / OpenAI 配置 ---
-API_KEY = "sk-86db1656b5a34daf8d856c58dd064530"  # 你的真实 Key
-API_BASE = "https://api.deepseek.com"
-MODEL_NAME = "deepseek-chat"
+API_KEY = ""  
+API_BASE = ""
+MODEL_NAME = ""
 
 # --- 2. 邮箱发送配置 ---
-SMTP_SERVER = "smtp.163.com"
-SMTP_PORT = 465
-SENDER_EMAIL = "ahmulwh@163.com"
-SENDER_PASS = "AL2itGnWK8MgcGfi"  # 你的真实授权码
+SMTP_SERVER = ""
+SMTP_PORT = 
+SENDER_EMAIL = ""
+SENDER_PASS = ""  # 你的真实授权码
 
 # --- 3. 接收人列表 ---
 # 可以是单个字符串，也可以是列表
-RECEIVERS = "ahmulwh@163.com"
+RECEIVERS = ""
 
 
 # --- 4. 搜索参数 ---
@@ -29,4 +29,4 @@ MAX_RESULTS = 20
 BATCH_SIZE = 5
 
 # --- 5. PUBMED ---
-PUBMED_EMAIL = "lwh20000531@gmail.com"
+PUBMED_EMAIL = ""
